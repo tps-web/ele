e59e69101913a8b5f9da293df7273338b0c4836f
tps-web <850671187@qq.com>
2020-01-13 17:26:53 +0800

    测试提交一个文件

--- src/pages/MSite/MSite.vue
+++ src/pages/MSite/MSite.vue
@@ -49,97 +49,94 @@
 </template>
 
 <script>
-  import BScroll from 'better-scroll'
-  import {mapState} from 'vuex'
-  import Swiper from 'swiper'
-  import 'swiper/dist/css/swiper.min.css'
+import BScroll from 'better-scroll'
+import {mapState} from 'vuex'
+import Swiper from 'swiper'
+import 'swiper/dist/css/swiper.min.css'
 
-  import HeaderTop from '../../components/HeaderTop/HeaderTop.vue'
-  import ShopList from '../../components/ShopList/ShopList.vue'
+import HeaderTop from '../../components/HeaderTop/HeaderTop.vue'
+import ShopList from '../../components/ShopList/ShopList.vue'
 
+export default {
+  data () {
+    return {
+      baseImageUrl: 'https://fuss10.elemecdn.com'
+    }
+  },
+  mounted () {
+    this.$store.dispatch('getCategorys')
+    this.$store.dispatch('getShops')
+  },
 
+  computed: {
+    ...mapState(['address', 'categorys', 'userInfo']),
 
-  export default {
-    data () {
-      return {
-        baseImageUrl: 'https://fuss10.elemecdn.com'
-      }
-    },
-    mounted () {
-
-      this.$store.dispatch('getCategorys')
-      this.$store.dispatch('getShops')
-    },
-
-    computed: {
-      ...mapState(['address', 'categorys', 'userInfo']),
-
-      /*
+    /*
       根据categorys一维数组生成一个2维数组
       小数组中的元素个数最大是8
        */
-      categorysArr () {
-        const {categorys} = this
-        // 准备空的2维数组
-        const arr = []
-        // 准备一个小数组(最大长度为8)
-        let minArr = []
-        // 遍历categorys
-        categorys.forEach(c => {
-          // 如果当前小数组已经满了, 创建一个新的
-          if(minArr.length===8) {
-            minArr = []
-          }
-          // 如果minArr是空的, 将小数组保存到大数组中
-          if(minArr.length===0) {
-            arr.push(minArr)
-          }
-          // 将当前分类保存到小数组中
-          minArr.push(c)
-        })
+    categorysArr () {
+      const {categorys} = this
+      // 准备空的2维数组
+      const arr = []
+      // 准备一个小数组(最大长度为8)
+      let minArr = []
+      // 遍历categorys
+      categorys.forEach(c => {
+        // 如果当前小数组已经满了, 创建一个新的
+        if (minArr.length === 8) {
+          minArr = []
+        }
+        // 如果minArr是空的, 将小数组保存到大数组中
+        if (minArr.length === 0) {
+          arr.push(minArr)
+        }
+        // 将当前分类保存到小数组中
+        minArr.push(c)
+      })
 
-        return arr
-      }
-    },
-
-    watch: {
-      categorys (value) { // categorys数组中有数据了, 在异步更新界面之前执行
-        // 使用setTimeout可以实现效果, 但不是太好
-        /*setTimeout(() => {
-          // 创建一个Swiper实例对象, 来实现轮播
-          new Swiper('.swiper-container', {
-            loop: true, // 可以循环轮播
-            // 如果需要分页器
-            pagination: {
-              el: '.swiper-pagination',
-            },
-          })
-        }, 100)*/
-
-        // 界面更新就立即创建Swiper对象
-        this.$nextTick(() => {// 一旦完成界面更新, 立即调用(此条语句要写在数据更新之后)
-          // 创建一个Swiper实例对象, 来实现轮播
-          new Swiper('.swiper-container', {
-            loop: true, // 可以循环轮播
-            // 如果需要分页器
-            pagination: {
-              el: '.swiper-pagination',
-            },
-          })
-
-          new BScroll('.miste-content-wrapper', {
-            click: true
-          })
-        })
-
-      }
-    },
-
-    components: {
-      HeaderTop,
-      ShopList
+      return arr
     }
+  },
+
+  watch: {
+    categorys (value) { // categorys数组中有数据了, 在异步更新界面之前执行
+      // 使用setTimeout可以实现效果, 但不是太好
+      /* setTimeout(() => {
+          // 创建一个Swiper实例对象, 来实现轮播
+          new Swiper('.swiper-container', {
+            loop: true, // 可以循环轮播
+            // 如果需要分页器
+            pagination: {
+              el: '.swiper-pagination',
+            },
+          })
+        }, 100) */
+
+      // 界面更新就立即创建Swiper对象
+      this.$nextTick(() => { // 一旦完成界面更新, 立即调用(此条语句要写在数据更新之后)
+        // 创建一个Swiper实例对象, 来实现轮播
+        new Swiper('.swiper-container', {
+          loop: true, // 可以循环轮播
+          // 如果需要分页器
+          pagination: {
+            el: '.swiper-pagination'
+          }
+        })
+
+        new BScroll('.miste-content-wrapper', {
+          click: true
+        })
+      })
+    }
+  },
+
+  components: {
+    HeaderTop,
+    ShopList
   }
+}
+// 添加注释
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
@@ -201,4 +198,5 @@
             color #999
             font-size 14px
             line-height 20px
+
 </style>
